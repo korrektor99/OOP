f830d9b7e6817b122362dd43e6292816c4ec441d
2 1 <korrektorrussia@mail.ru>
2023-03-07 17:06:14 +0700

    dz done

---
+++ Java_OOP_HW22/Tree/src/Finder.java
@@ -0,0 +1,8 @@
+package Java_OOP_HW22.Tree.src;
+import java.util.List;
+public interface Finder {
+    public List getParents();
+    public List getChildren();
+    public List getSubParents();
+    public List getSubChildren();
+}
---
+++ Java_OOP_HW22/Tree/src/TreeNode.java
@@ -0,0 +1,29 @@
+package Java_OOP_HW22.Tree.src;
+public abstract class TreeNode {
+    private String name;
+    private String surname;
+
+    private TreeNode parent;
+
+    private TreeNode child;
+
+    public void getParent(){}
+
+    public void setParent(){}
+
+    public void getChild(){}
+
+    public void setChild(){}
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public String getNameData(){
+        return "Person is " + name + " " + surname;
+    }
+}
